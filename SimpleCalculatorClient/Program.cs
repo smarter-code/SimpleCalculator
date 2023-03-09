990a11d52dd51c0f7c27a162b7f8d5eca3b92a3e
Mohammed Osman <mohammed.osman@smartercode.io>
2023-03-09 23:37:03 +0100

    Added a simple client class

---
+++ SimpleCalculatorClient/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Welcome to the Simple Calculator");
+Console.WriteLine("--------------------------------");
+
+var simpleCalculator = new SimpleCalculator.SimpleCalculator();
+
+Console.WriteLine($"1 + 5 is {simpleCalculator.Add(1, 5)}");
+Console.WriteLine($"2 * 5 is {simpleCalculator.Multiply(2, 5)}");
+Console.WriteLine($"12 - 3 is {simpleCalculator.Subtract(12, 3)}");
+Console.WriteLine($"6 + 4 is {simpleCalculator.Add(6, 4)}");
+Console.WriteLine("--------------------------------");
+Console.WriteLine("Bye Bye Simple Calculator!");
+
