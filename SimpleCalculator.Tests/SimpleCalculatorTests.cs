8a923ad905a0c9984a9fa4b334c6738579a746ce
Mohammed Osman <mohammed.osman@smartercode.io>
2023-03-09 22:55:27 +0100

    Add project files.

---
+++ SimpleCalculator.Tests/SimpleCalculatorTests.cs
@@ -0,0 +1,53 @@
+namespace SimpleCalculator.Tests
+{
+    public class SimpleCalculatorTests
+    {
+        private readonly SimpleCalculator _calculator;
+
+        public SimpleCalculatorTests()
+        {
+            _calculator = new SimpleCalculator();
+        }
+
+        [Fact]
+        public void Add_ReturnsCorrectResult()
+        {
+            // Arrange
+            int x = 4;
+            int y = 6;
+            int expected = 10;
+
+            // Act
+            int actual = _calculator.Add(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Subtract_ReturnsCorrectResult()
+        {
+            // Arrange
+            int x = 10;
+            int y = 4;
+            int expected = 6;
+
+            // Act
+            int actual = _calculator.Subtract(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Divide_DivideByZero_ThrowsException()
+        {
+            // Arrange
+            int x = 10;
+            int y = 0;
+
+            // Act and Assert
+            Assert.Throws<DivideByZeroException>(() => _calculator.Divide(x, y));
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,30 @@
+﻿namespace SimpleCalculator
+{
+    public class SimpleCalculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return x / y;
+        }
+    }
+}
