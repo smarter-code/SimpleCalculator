5bfa2d00d9afba5177e96f67ce3909fdbc73458e
Mohammed Osman <mohdrash1990@hotmail.com>
2023-04-29 13:12:47 +0000

    Added a bye bye line

--- SimpleCalculatorClient/Program.cs
+++ SimpleCalculatorClient/Program.cs
@@ -9,4 +9,5 @@ Console.WriteLine($"12 - 3 is {simpleCalculator.Subtract(12, 3)}");
 Console.WriteLine($"6 + 4 is {simpleCalculator.Add(6, 4)}");
 Console.WriteLine("--------------------------------");
 Console.WriteLine("Bye Bye Simple Calculator!");
+Console.WriteLine("A new line from Gitpod!");
 
